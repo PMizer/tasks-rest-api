1f3a31b60493ea74f978bd2b11e26fe5de3f9b2a
“se1ryu <mizer.piotr@gmail.com>
2018-06-12 13:03:03 +0200

    15.2 Tresc

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,12 +1,16 @@
 package com.crud.tasks.domain;
 
-import lombok.Getter;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class TaskDto {
+
     private Long id;
     private String title;
     private String content;
+
 }
